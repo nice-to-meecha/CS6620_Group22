147261ae7a3fb48918d51c6abf88fee6dc2cb1e3
bduwuriexit <duvvuribhavya@gmail.com>
2022-04-27 23:29:26 +0000

    adding config files to S3

--- s3_bucket.py
+++ s3_bucket.py
@@ -12,11 +12,27 @@ for the Home Assistant container.
 
 import boto3, logging
 from botocore.exceptions import ClientError
+from glob import glob
 
-S3 = boto3.client('s3', region_name = 'us-east-1')
 
-def main():
-    pass
+s3 = boto3.client('s3')
+
+
+def upload_func(file, bucket, object_name=None):
+    #print(1)
+    if object_name is None:
+        object_name = os.path.basename(file)
+        #s3 = boto3.client('s3')
+    try:
+        response = s3.upload_file(file, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+def main(): 
+    s3.create_bucket(Bucket='config-home-assistant')
+    for file_name in glob('ha_config_files/*.yaml'):
+        upload_func(file_name, 'config-home-assistant', file_name)
 
 
 
