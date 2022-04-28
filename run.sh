d9841b31c81bfc96cb072ce09a1bd1a05febc926
Stamesha Bello <stameshabello@gmail.com>
2022-04-28 01:45:05 +0000

    Using CLI args for s3_bucket.py; added tags

--- run.sh
+++ run.sh
@@ -19,47 +19,57 @@ GROUP22_ARN="arn:aws:iam::911656642006:user/stameshabello_butnotstameshabello"
 
 
 # Installing kubectl
-echo "Installing kubectl..."
-curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.22.6/2022-03-09/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
-kubectl version --short --client
-echo ""
+#echo "Installing kubectl..."
+#curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.22.6/2022-03-09/bin/linux/amd64/kubectl
+#chmod +x ./kubectl
+#mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+#kubectl version --short --client
+#echo ""
 
 
 # Installing eksctl
-echo "Installing eksctl..."
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-sudo mv /tmp/eksctl /usr/local/bin
-eksctl version
-echo ""
+#echo "Installing eksctl..."
+#curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+#sudo mv /tmp/eksctl /usr/local/bin
+#eksctl version
+#echo ""
 
 
+# Creates roles needed for EKS control panel and worker nodes
 cluster_role_name="Group22_EKSClusterRole"
 cluster_role_tag="cs6620-group22-eks-cluster-role"
 node_role_name="Group22_EKSNodeRole"
 node_role_tag="cs6620-group22-eks-node-role"
-python3 create_roles.py $cluster_role_name $cluster_role_tag $node_role_name $node_role_tag
+#python3 create_roles.py $cluster_role_name $cluster_role_tag $node_role_name $node_role_tag
 
 
+# Creates EC2 Launch Template (for EKS worker nodes)
 template_name="Group22_EKS_EC2_launch_template"
 template_tag="cs6620-group22-eks-ec2-launch-template"
 template_key="stamesha-bello-key"
 S3_bucket_name="homeassistant-config"
-python3 launch_template.py $template_name $template_tag $template_key $S3_bucket_name
+#python3 launch_template.py $template_name $template_tag $template_key $S3_bucket_name
 
 
+# Creates cloud stack, with networking resources (e.g. VPC, subnets, security groups, igw, etc)
 stack_name="Group22-CloudFormation-EKS-Stack"
 stack_tag="cs6620-group22-cloudformation-eks-stack"
-python3 cloudstack.py $stack_name $stack_tag
+#python3 cloudstack.py $stack_name $stack_tag
 
 
+# Creates database to store Home Assistant's data
 db_instance_name="Group22-RDS-database-instance"
 db_name="homeassistant"
 username="bello"
 password="password"
-python3 database.py $stack_name $db_instance_name $db_name $username $password
+#python3 database.py $stack_name $db_instance_name $db_name $username $password
 
+
+# Creates S3 bucket to store Home Assistant's config files
+s3_bucket_name="group22-home-assistant-config-bucket"
+s3_bucket_tag="cs6620-group22-config-s3-bucket"
+config_file_dir="ha_config_files/"
+python3 s3_bucket.py $s3_bucket_name $s3_bucket_tag $config_file_dir
 
 
 #eks_cluster="Group22-EKS-cluster"
--- s3_bucket.py
+++ s3_bucket.py
@@ -10,7 +10,7 @@ This script will generate an S3 bucket, retaining necessary config files
 for the Home Assistant container.
 '''
 
-import boto3, logging
+import boto3, logging, sys, os
 from botocore.exceptions import ClientError
 from glob import glob
 
@@ -18,23 +18,103 @@ from glob import glob
 s3 = boto3.client('s3')
 
 
-def upload_func(file, bucket, object_name=None):
-    #print(1)
+
+def create_bucket(name: str, tag: str):
+    '''
+    Creates an S3 bucket, with the provided name and tag
+
+    :param name: The name of the S3 bucket
+    :param tag: The tag for the S3 bucket
+
+    :return: None
+    '''
+
+    try:
+        s3.create_bucket(Bucket = name)
+        s3.put_bucket_tagging(
+                Bucket = name,
+                Tagging = {
+                    'TagSet': [
+                        {
+                            'Key': 'Name',
+                            'Value': tag
+                            }
+                        ]
+                    }
+                )
+
+    except ClientError as e:
+        logging.error(e)
+
+
+
+def upload_file(file: str, bucket: str, object_tag: str, object_name: str = None):
+    '''
+    Uploads the provided file within the indicated S3 bucket
+
+    :param file: The name of the file to be uploaded into an S3 bucket
+    :param bucket: The name of the S3 bucket, in which the provided file
+                   will be uploaded
+    :param object_tag: The tag of the newly uploaded file/object
+    :param object_name: The name that the file/object will utilize, after being
+                        uploaded in the provided bucket
+    '''
+
     if object_name is None:
         object_name = os.path.basename(file)
-        #s3 = boto3.client('s3')
+    
     try:
-        response = s3.upload_file(file, bucket, object_name)
+        s3.upload_file(file, bucket, object_name)
+        s3.put_object_tagging(
+                Bucket = bucket,
+                Key = object_name,
+                Tagging = {
+                    'TagSet': [
+                        {
+                            'Key': 'Name',
+                            'Value': object_tag
+                            }
+                        ]
+                    }
+                )
+
     except ClientError as e:
         logging.error(e)
         return False
+
     return True
-def main(): 
-    s3.create_bucket(Bucket='config-home-assistant')
-    for file_name in glob('ha_config_files/*.yaml'):
-        upload_func(file_name, 'config-home-assistant', file_name)
+
+
+
+def main():
+    '''
+    Creates an S3 bucket and uploads the config files for Home Assistant,
+    using the following command line arguments:
+
+    s3_bucket.py <bucket_name> <bucket_tag>
+
+    :CLI arg bucket_name: The name of the S3 bucket to be generated
+    :CLI arg bucket_tag: The tag for the S3 bucket
+    :CLI arg config_file_dir: The relative directory for Home Assistant's
+                              config files
+    '''
+
+    bucket_name = sys.argv[1]
+    bucket_tag = sys.argv[2]
+    create_bucket(bucket_name, bucket_tag)
+
+    config_file_dir = sys.argv[3]
+    if (config_file_dir[-1] != "/"):
+        config_file_dir = config_file_dir + "/"
+        
+    config_file_dir = config_file_dir + "*.yaml"
+    object_tag = bucket_tag + "-object"
+
+    for file_name in glob(config_file_dir):
+        upload_file(file_name, bucket_name, object_tag, file_name)
 
 
 
 if __name__ == "__main__":
     main()
+
